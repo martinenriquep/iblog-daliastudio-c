4a92f39340628539517448f8563d99df95132661
martinenriquep <martinenrique.pe@gmail.com>
2022-10-17 18:11:44 -0500

    cambio config

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,9 +15,9 @@ module.exports = {
     {
       resolve: "gatsby-source-contentful",
       options: {
-        spaceId: process.env.CONTENTFUL_SPACE_ID,
-        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
-        host: process.env.CONTENTFUL_HOST
+        spaceId: "py3grjtw5y7d",
+        accessToken: "MDmFFni38Kl8RlHXHyi3ijrNatc3bKNN_zYRSrfKjcs",
+        host: "preview.contentful.com"
       },
     },
   ],
